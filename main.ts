85bbb1a7a7eb2756c38bda70841241114f1699fc
Takumi Okawa <takunology.dev@gmail.com>
2026-06-15 18:09:36 +0900

    Simplify hiragana movement block

--- main.ts
+++ main.ts
@@ -1,47 +1,13 @@
 //% block="ひらがな"
 //% color="#4b7bec"
 namespace hiragana {
-    export enum MoveDirection {
-        //% block="まえ"
-        Forward,
-        //% block="うしろ"
-        Back,
-        //% block="ひだり"
-        Left,
-        //% block="みぎ"
-        Right,
-        //% block="うえ"
-        Up,
-        //% block="した"
-        Down
-    }
-
     /**
-     * エージェントを、えらんだほうこうにうごかします。
+     * エージェントを、まえにうごかします。
      */
-    //% blockId=hiragana_agent_move
-    //% block="エージェントを %direction に %distance ブロック うごかす"
+    //% blockId=hiragana_agent_move_forward
+    //% block="エージェントを まえに $distance ブロック うごかす"
     //% distance.min=1 distance.defl=1
-    export function moveAgent(direction: MoveDirection, distance: number): void {
-        switch (direction) {
-            case MoveDirection.Forward:
-                agent.move(FORWARD, distance)
-                break
-            case MoveDirection.Back:
-                agent.move(BACK, distance)
-                break
-            case MoveDirection.Left:
-                agent.move(LEFT, distance)
-                break
-            case MoveDirection.Right:
-                agent.move(RIGHT, distance)
-                break
-            case MoveDirection.Up:
-                agent.move(UP, distance)
-                break
-            case MoveDirection.Down:
-                agent.move(DOWN, distance)
-                break
-        }
+    export function moveAgentForward(distance: number): void {
+        agent.move(FORWARD, distance)
     }
 }
